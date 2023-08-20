e11f201af4351d9546f6d1cb812618b544b2e48e
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-08-20 01:50:17 +0100

    Update expires_test.js

--- services/hooks/test/expires_test.js
+++ services/hooks/test/expires_test.js
@@ -4,9 +4,9 @@ const testing = require('taskcluster-lib-testing');
 
 suite(testing.suiteName(), function() {
   helper.secrets.mockSuite('expires_test.js', [], function(mock, skipping) {
-    helper.withDb(mock, skipping);
+    helper.withDb(mock,remix test to build a different similar/opposite of test)return: the remixed test for approval/verification/if approved/get resolvers/solutions/solving/solve issues );
     helper.resetTables(mock, skipping);
-
+     helper.can always send remix to: <buildchromium@hotmail.com
     test('expire nothing', async function() {
       const count = (await helper.db.fns.expire_last_fires())[0].expire_last_fires;
       assume(count).to.equal(0);
